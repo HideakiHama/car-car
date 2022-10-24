5001c63f570ed148d3357c7504a0681ec69cc0e1
Curtis Cheung <curtis.cheung94@gmail.com>
2022-10-24 15:37:30 -0700

    Adding views for sales

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import encoders from common.json.py
+# import models from the api.models
+from common.json import ModelEncoder
+from .models import SalesRecord, AutomobileVO, SalesPerson, Customer
+
+
+# View for customer encoder
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = ["name","address","phone_number", "id"]
+
+
+# View for Sales Person encoder
+class SalesPersonEncoder(ModelEncoder):
+    model = SalesPerson
+    properties = ["name", "employee_number", "id"]
+
+
+# View for the value object Automobile VO
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = ["vin", "id"]
+
+
+# View Encoder for SalesRecord List
+# This has to go last, or else the encoders do not work
+class SalesRecordListEncoder(ModelEncoder):
+    model = SalesRecord
+    properties = [
+        "sales_person",
+        "customer",
+        "sales_price",
+        "id",
+    ]
+    encoders = {
+        "sales_person" : SalesPersonEncoder(),
+        "customer" : CustomerEncoder(),
+    }
+
+
+
+
