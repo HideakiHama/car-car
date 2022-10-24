50146ff5ff912280cacd17594f2eb0fc2ee9d6b1
Hideaki Hama <hideaki.hama@yahoo.com>
2022-10-24 15:34:19 -0700

    Added Model for Service

--- README.md
+++ README.md
@@ -3,14 +3,22 @@
 Team:
 
 * Person 1 - Which microservice?
+ Curtis - Sales
 * Person 2 - Which microservice?
+ Aki - Service
 
 ## Design
 
+
+
 ## Service microservice
+
 
 Explain your models and integration with the inventory
 microservice, here.
+
+
+
 
 ## Sales microservice
 
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class AutomobileVO(models.Models):
+    vin = models.CharField(max_length=17, unique=True)
+
+class Technician(models.Models):
+    name = models.CharField(max_length=200)
+    employee_number = models.IntegerField(unique=True)
+
+
+class ServiceAppointment(models.Models):
+    customer_name = models.CharField(max_length=200)
+    date_app = models.DateField()
+    time_app = models.TimeField(auto_now_add=True)
+    reason = models.CharField(max_length=200)
+    technician = models.ForeignKey(Technician name ="ServiceAppointment" on_delete=models.CASCADE)
+    Vin = models.ForeignKey(AutomobileVO, name="CostomerVin" on_delete=models.CASCADE)
