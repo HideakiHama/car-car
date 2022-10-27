0d9f25bd97255168dc8ce845a9b70f886358000b
Curtis Cheung <curtis.cheung94@gmail.com>
2022-10-27 16:32:48 -0700

    class=className in Naj.js

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,19 +10,19 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="#">Sales</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">Sales</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                  <ul className="navbar-nav">
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         
                       </a>
-                      <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                      <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                         <li><NavLink className="dropdown-item" aria-current="page" to="customers/new/">New Customers</NavLink></li>
                         <li><NavLink className="dropdown-item" aria-current="page" to="sales-persons/new/">New Employee</NavLink></li>
                         <li><NavLink className="dropdown-item" aria-current="page" to="sales-records/new/">New Sales!</NavLink></li>
@@ -34,19 +34,19 @@ function Nav() {
                 </div>
               </div>
             </nav>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="#">Inventory</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">Inventory</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                  <ul className="navbar-nav">
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         
                       </a>
-                      <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                      <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                       <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers/new/">New Manufacturer</NavLink></li>
                       <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers/">All Manufacturers</NavLink></li>
                       <li><NavLink className="dropdown-item" aria-current="page" to="models/new/">New Vehicle Model</NavLink></li>
@@ -59,19 +59,19 @@ function Nav() {
                 </div>
               </div>
             </nav>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="#">Service</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">Service</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                  <ul className="navbar-nav">
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         
                       </a>
-                      <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                      <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                         {/* <li><NavLink className="dropdown-item" aria-current="page" to="customers/new/">New Customers</NavLink></li>
                         <li><NavLink className="dropdown-item" aria-current="page" to="sales-persons/new/">New Sales Representative</NavLink></li>
                         <li><NavLink className="dropdown-item" aria-current="page" to="sales-records/new/">New Sales Records</NavLink></li>
